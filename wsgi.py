6c3b510af463290eff412e2e42ef4cce29e6508c
guilherme_celente <guilherme.celente@outlook.com>
2025-06-20 16:48:18 -0300

    build: ajuste de erro ao fazer deploy

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,7 @@ from app import create_app
 import os
 
 # Cria a instância da aplicação
-app = create_app(os.getenv('FLASK_ENV', 'development'))
+app = create_app('development')
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
